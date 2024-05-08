c595656656b6ee2e253fb0f02b55e0cd7dda975f
Rasa <rasa.zukauskaite@gmail.com>
2024-05-08 19:22:11 +0300

    update homework

--- js/ciklai/for.js
+++ js/ciklai/for.js
@@ -7,8 +7,70 @@ i++ tas pats kas i= i+1, arba i+=1 arba ++i
 
 console.log('STARTAS');
 
-for (let i = 3; i<8; i++) {
-console.log(i);
+for (let i = 3; i < 8; i++) {
+    console.log(i);
 }
 
 console.log('FINISAS');
+
+console.clear();
+
+const colors = ['white', 'black', 'red', 'green', 'blue', 'violet'];
+
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(i, colors[i]);
+
+}
+
+console.log('--------');
+
+//is galo i prieki israsymas
+//length istrauks neteisinga (6), tuomet reikia minusuot 1
+// i>=0 kad paimtu pirma reiksme arba i > -1
+
+for (let i = colors.length - 1; i >= 0; i--) {
+    console.log(i, colors[i]);
+}
+
+console.log('-----');
+//ciklas eina mazejimo tvarka, o reiksmes didejimo
+
+for (let i = colors.length - 1; i >= 0; i--) {
+    console.log(i, colors[colors.length - 1 - i]);
+}
+
+console.clear();
+
+const marks = [10, 2, 8, 4, 6];
+
+let sum = 0;
+for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+
+}
+console.log(sum / marks.length);
+
+console.clear();
+
+const jonas = [];
+const maryte = [10, 2];
+const petras = [9, 8, 7];
+const ona = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function marksAverage(marks) {
+    if (marks.length === 0) {
+        return 'Nera pazymiu';
+    }
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    return sum / marks.length;
+}
+
+console.log(marksAverage(jonas));
+console.log(marksAverage(maryte));
+console.log(marksAverage(petras));
+console.log(marksAverage(ona));
---
+++ js/namudarbai.js
@@ -0,0 +1,275 @@
+//Namu darbai - Mini uzduotys
+
+//Kintamuju inicijavimas ir veiksmai
+
+//1 su skaiciaus tipo reiksmemis
+
+
+const x = 3;
+console.log(x);
+const y = 9;
+console.log(y);
+const z = 5;
+console.log(z);
+
+//susumuoti visus skaiciaus kintamuosius
+
+const sum99 = x + y + z;
+console.log(sum99);
+
+//2 su teksto tipo reiksmemis
+const string1 = 'Rasa';
+console.log(string1);
+
+const string2 = 'Gabriele';
+console.log(string2);
+
+const string3 = 'Zukauskaite';
+console.log(string3);
+
+//sujungti visus teksto kintamuosius su tarpais.
+const text = string1 + ' ' + string2 + ' ' + string3;
+console.log(text);
+
+
+//3 saraso tipo kintamieji su 5 skaiciu tipo reiksmemis
+
+const marks1 = [3, 5, 7, 9, 15];
+console.log(marks1);
+
+//apskaiciuoti verte pagal logika 1-2+3-4+5
+const logika1 = marks1[0] - marks1[1] + marks1[2] - marks1[3] + marks1[4];
+console.log(logika1);
+
+const marks2 = [1, 3, 5, 7, 9];
+console.log(marks2);
+
+//apskaiciuoti verte pagal logika 1-2+3-4+5
+const logika2 = marks2[0] - marks2[1] + marks2[2] - marks2[3] + marks2[4];
+console.log(logika2);
+
+const marks3 = [2, 4, 6, 8, 10];
+console.log(marks3);
+
+//apskaiciuoti verte pagal logika 1-2+3-4+5
+
+const logika3 = marks3[0] - marks3[1] + marks3[2] - marks3[3] + marks3[4];
+console.log(logika3);
+
+
+//4 sukurti 3 saraso tipo kintam. su 5 teksto reiksmemis
+
+let marksT1 = ['liepa', 'uosis', 'tuja', 'kadagys', 'azuolas'];
+
+console.log(marksT1);
+
+//sujungti saraso vertes su tarpu ir kableliu atbuline tvarka.
+
+console.log(marksT1[4] + ', ' + marksT1[3] + ', ' + marksT1[2] + ', ' + marksT1[1] + ', ' + marksT1[0]);
+
+let marksT2 = ['tulpe', 'narcizas', 'ramune', 'roze', 'bijunas'];
+console.log(marksT2);
+
+//sujungti saraso vertes su tarpu ir kableliu.
+
+console.log(marksT2[4] + ', ' + marksT2[3] + ', ' + marksT2[2] + ', ' + marksT2[1] + ', ' + marksT2[0]);
+
+const marksT3 = ['morka', 'svogunas', 'salota', 'ridikas', 'krapai'];
+console.log(marksT3);
+
+//sujungti saraso vertes su tarpu ir kableliu.
+
+console.log(marksT3[4] + ', ' + marksT3[3] + ', ' + marksT3[2] + ', ' + marksT3[1] + ', ' + marksT3[0]);
+
+
+
+//KINTAMUJU PALYGINIMAS
+
+//Skaiciaus tipo kintamieji
+
+const skaicius1 = 7;
+const skaicius2 = 9;
+
+if (skaicius1 > skaicius2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');  //a skaiciaus tipo kuris didesnis
+}
+
+if (skaicius1 < skaicius2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');  //b skaiciaus tipo kuris mazesnis
+}
+
+
+if (skaicius1 === skaicius2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');  //c ar jie lygus
+}
+
+
+if (skaicius1 != skaicius2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');  //d ar jie nelygus
+}
+
+if (skaicius1 >= skaicius2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');  //e kuris didesnis arba lygus
+}
+
+if (skaicius1 <= skaicius2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');  //e kuris mazesnis arba lygus
+}
+
+
+//2. isvesti teksto tipo kintamuju ilgius
+
+console.log(string1, string1.length);
+
+console.log(string2, string2.length);
+
+console.log(string3, string3.length);
+
+//3. Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+
+//a kuris didesnis
+
+if (string1.length < string2.length) {
+    console.log('taip');
+} else {
+    console.log('Ne');
+}
+//b kuris mazesnis
+if (string1.length > string2.length) {
+    console.log('taip');
+} else {
+    console.log('Ne');
+}
+
+//c ar jie lygus
+if (string1.length > string2.length) {
+    console.log('taip');
+} else {
+    console.log('Ne');
+}
+
+//d ar jie nelygus
+
+
+//e kuris didesnis arba lygus
+if (string1.length >= string2.length) {
+    console.log('taip');
+} else {
+    console.log('Ne');
+}
+
+//f kuris mazesnis arba lygus
+if (string1.length <= string2.length) {
+    console.log('taip');
+} else {
+    console.log('Ne');
+}
+
+//4. Isvesti saraso tipo kintamuju ilgius
+console.log(marksT1, marksT1.length);
+
+console.log(marksT2, marksT2.length);
+console.log(marksT3, marksT3.length);
+
+
+//5. Tarpusavyje palyginti saraso tipo kintamuju ilgius
+
+//a, kuris didesnis
+
+//b, kuris mazesnis
+
+//c, ar jie lygus
+
+//d, ar jie nelygus
+
+//e, kuris didesnis arba lygus
+
+//f, kuris mazesnis arba lygus
+
+console.clear();
+
+
+//CIKLO FOR PANAUDOJIMAS
+
+
+//1 suskaiciuoti suma imtinai '0...0'
+let sum = 0;
+for (let i = 0; i <= 0; i++) {
+}
+console.log(sum);
+
+
+
+//suskaiciuoti suma imtinai '0...4'
+
+let ciklas = 0;
+for (let i = 0; i <= 4; i++) {
+    ciklas += i;
+}
+console.log(ciklas);
+
+
+//suskaiciuoti suma imtinai '0...100'
+let simtas = 0;
+for (let i = 0; i <= 100; i++) {
+    simtas += i;
+}
+console.log(simtas);
+
+
+
+//suskaiciuoti suma imtinai '574...815'
+let skaicius = 0;
+
+for (let i = 574; i <= 815; i++) {
+    skaicius += i;
+}
+console.log(skaicius);
+
+
+//suskaiciuoti suma imtinai '-50...50'
+let penki = 0;
+for (let i = -50; i <= 50; i++) {
+    penki += i;
+}
+console.log(penki);
+
+
+//suskaiciuoti suma imtinai '-70...30'
+
+let naujas = 0;
+for (let i = -70; i <= 30; i++) {
+    naujas += i;
+}
+console.log(naujas);
+
+console.log('--------');
+//KITAS BUDAS
+function intervalSum(startas, pabaiga) {
+    let answer = 0;
+
+    for (let i = startas; i <= pabaiga; i++) {
+        answer += i;
+
+    }
+
+    return answer;
+}
+console.log(intervalSum(0, 0));
+console.log(intervalSum(0, 4));
+console.log(intervalSum(0, 100));
+console.log(intervalSum(574, 815));
+console.log(intervalSum(-50, 50));
+console.log(intervalSum(-70, 30));
